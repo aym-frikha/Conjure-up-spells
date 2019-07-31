d216f4bfa67a71a3afa1647421738b4cc1540365
Ubuntu <ubuntu@node09ob32.maas>
2019-07-31 21:15:26 +0000

    first commit

---
+++ openstack-novalxd-custom/steps/01_glance/glance.sh
@@ -0,0 +1,36 @@
+imagetype=root.tar.xz
+diskformat=raw
+imagesuffix="-lxd"
+
+GLANCE_LOG="glance.log"
+
+mkdir -p "$HOME/glance-images" || true
+if [ ! -f "$HOME/glance-images/xenial-server-cloudimg-amd64-$imagetype" ]; then
+    debug "Downloading xenial image..."
+    wget --user-agent="conjure-up/openstack-novalxd" -qO ~/glance-images/xenial-server-cloudimg-amd64-$imagetype https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-$imagetype
+fi
+if [ ! -f "$HOME/glance-images/bionic-server-cloudimg-amd64-$imagetype" ]; then
+    debug "Downloading bionic image..."
+    wget --user-agent="conjure-up/openstack-novalxd" -qO ~/glance-images/bionic-server-cloudimg-amd64-$imagetype https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64-$imagetype
+fi
+
+if ! glance image-list --property-filter name="bionic$imagesuffix" | grep -q "bionic$imagesuffix" ; then
+    debug "Importing bionic$imagesuffix"
+    glance image-create --name="bionic$imagesuffix" \
+           --container-format=bare \
+           --disk-format=$diskformat \
+           --property architecture="x86_64" \
+           --visibility=public --file="$HOME/glance-images/bionic-server-cloudimg-amd64-$imagetype" >> $GLANCE_LOG 2>&1
+fi
+if ! glance image-list --property-filter name="xenial$imagesuffix" | grep -q "xenial$imagesuffix" ; then
+    debug "Importing xenial$imagesuffix"
+    glance image-create --name="xenial$imagesuffix" \
+           --container-format=bare \
+           --disk-format=$diskformat \
+           --property architecture="x86_64" \
+           --visibility=public --file="$HOME/glance-images/xenial-server-cloudimg-amd64-$imagetype" >> /dev/null 2>&1
+fi
+
+openstack flavor create --id 1 --ram 2048 --disk 8 --vcpus 1 m1.tiny >> $GLANCE_LOG 2>&1
+
+printf "Glance images for Bionic (18.04) and Xenial (16.04) are imported and accessible via Horizon dashboard."
---
+++ openstack-novalxd-custom/steps/03_neutron/neutron.sh
@@ -0,0 +1,31 @@
+# Get host namserver
+get_host_ns() {
+    perl -lne 's/^nameserver\s+// or next; s/\s.*//; print && exit' /etc/resolv.conf
+}
+
+NEUTRON_LOG="neutron.log"
+
+debug "Creating External Network"
+./neutron-ext-net --network-type flat \
+                  -g 10.101.0.1 \
+                  -c 10.101.0.0/24 \
+                  -f 10.101.0.10:10.101.0.254 ext_net >> $NEUTRON_LOG 2>&1
+
+debug "Creating Internal Network"
+./neutron-tenant-net -p admin -r provider-router \
+                     internal 10.5.5.0/24 >> $NEUTRON_LOG 2>&1
+
+debug "Setting security roles"
+
+sudo apt-get update -qq > /dev/null 2>&1
+sudo apt-get install -qyf jq > /dev/null 2>&1
+
+for secgroup_id in $(openstack security group list -f json|jq -r .[].ID); do
+    neutron security-group-rule-create --protocol icmp \
+            --direction ingress "$secgroup_id" >> $NEUTRON_LOG 2>&1
+    neutron security-group-rule-create --protocol tcp \
+        --port-range-min 22 --port-range-max 22 \
+        --direction ingress "$secgroup_id" >> $NEUTRON_LOG 2>&1
+done
+
+printf "Neutron networking is now configured and is available to you during instance creation."
